c924098165c93963554a0ab1b0e4cf84404d7f97
thomaslb <thomaslb@152.23.255.218>
2019-09-02 21:44:06 -0400

    Completely wrote the code

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -8,7 +8,35 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numCust = scan.nextInt();
+		String [] custNames = new String[numCust*2];
+		double [] custSums = new double[numCust];
+		for(int i = 0; i < numCust*2; i+=2)
+		{
+			int location = i;
+			addCust(scan.next(), custNames, location);
+			location = i + 1;
+			addCust(scan.next(), custNames, location);
+			
+			double sumItems = 0;	//total sum of values for items, reused for each customer
+			
+			for(int j = 0; j < scan.nextInt(); j++)
+			{
+				int numItems = scan.nextInt();
+				String notImportant = scan.next();
+				sumItems += scan.nextDouble() * numItems;
+				custSums[i/2] = sumItems;
+			}
+			
+		}
+		for(int i = 0; i < numCust*2; i+=2)
+		{
+			System.out.println(custNames[i].charAt(0)+". "+custNames[i+1]+": "+String.format("%.2f", custSums[i/2]));
+		}
 		
+	}
+	static void addCust(String name, String[] names, int location)
+	{
+		names[location] = name;
 	}
 }

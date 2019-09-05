cfa367f58fc1d04b439cc544282b366e683dbf81
thomaslb <thomaslb@152.23.255.218>
2019-09-04 22:57:46 -0400

    Final code

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -46,11 +46,11 @@ public class A1Jedi {
 		{
 			if(numCustBoughtItems[i] == 0)
 			{
-				System.out.println("No customers bought an " + itemsStore[i]);
+				System.out.println("No customers bought " + itemsStore[i]);
 			}
 			else
 			{
-				System.out.println(numCustBoughtItems[i] + " customers bought " + numItemsBought[i] + itemsStore[i]);
+				System.out.println(numCustBoughtItems[i] + " customers bought " + numItemsBought[i] + " " + itemsStore[i]);
 			}
 		}
 			//System.out.println("Biggest: " + custNames[bigSpender(custSums)*2] + " " + custNames[bigSpender(custSums)*2+1] + " (" + String.format("%.2f", custSums[bigSpender(custSums)]) + ")");
